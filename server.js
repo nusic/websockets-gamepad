7f6b8f81f576fe1709ec44416c981ee4fd28875d
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-02-17 15:02:53 +0100

    added commented code from Emil Axelsson and Erik Larssons project

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mod_ctype = require('ctype')
 /* Set up web server */
 var express = require('express'),
     WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: config.wsPort}),
+    wss = new WebSocketServer({port: config.wsPort}), //pass an object that has property "port" with value: config.wsPort
     app = express();
 
 app.get('/', function (req, res) {
@@ -63,27 +63,47 @@ server.on('close', function(data){
     //start to listen again
     this.listen(config.reportPort, config.reportHost);
 });
+
+
+
 /*
  * Set up WebSocket Server
  */
 
-var sockets = [];
+var sockets = []; //
 var nextSocketId = 0;
 var color = generateColor();
 
+/*
+    This function is called every time a client connects to the local server at
+        example: http://127.0.0.1:1337/
+    or
+        http:// <config.reportHost> : <config.reportPort> 
+        
+
+    The function takes a function as argument (which we will define), which in
+    turn has a websocket as argument. The server will have one websocket per client.
+*/
 wss.on('connection', function(ws) {
 
+    //When a client connects: give it an ID for this client/websocket
     var socketId = nextSocketId++;
+
+    //Store the websocket in array.
     sockets[socketId] = ws;
     ws.send(color);
     console.log(">>> Client connect with socketId: " + socketId);
+
+
+    //This function gets called every time a client sends a message. 
     ws.on('message', function(message) {
         console.log("---");
         console.log('message recieved: ' + message);
 
+        //when client sends message: randomize color on panel on server
         color = generateColor();
 
-
+        //update the color on panel for all clients
         Object.keys(sockets).forEach(function (k) {
             var v = sockets[k];
             if(v == ws)
@@ -105,7 +125,9 @@ wss.on('connection', function(ws) {
         });
     });
 
+    //this function gets called when client closes window
     ws.on('close', function(ws) {
+        //remove websocket from server
         sockets[socketId] = null;
     });
 })  ;
@@ -113,7 +135,6 @@ wss.on('connection', function(ws) {
 /*
 * Generate new color
 */
-
 function generateColor() {
     var c = Math.round(Math.random()*0xffffff).toString(16);
     while (c.length < 6) {
